867901c1facdd3dbe28fee5fe37c2e6e2af5f2a4
LiliMeng <lilimeng1103@gmail.com>
2016-07-09 17:50:00 -0700

    Update TUM27ScenesFormat.m

--- TUM27ScenesFormat.m
+++ TUM27ScenesFormat.m
@@ -1,11 +1,23 @@
 close all
 clear all
-PoseTQ=load('/Users/jimmy/Desktop/images/TUM_SLAM/freiburg3_long_office_household/seq-01/frame-000000.pose.quaternion.txt')
 
-PoseRT=PoseTQ2PoseRT(PoseTQ)
+PoseFileName=dir('/Users/jimmy/Desktop/images/TUM_SLAM/freiburg3_long_office_household/seq-01/*.txt');
 
-fileID=fopen('/Users/jimmy/Desktop/images/TUM_SLAM/freiburg3_long_office_household/seq-01/frame-000000.pose.txt','w');
+numFiles=size(PoseFileName,1);
 
-for i=1:4
-    fprintf(fileID, '%.10f %.10f %.10f %10f \n', PoseRT(i,1), PoseRT(i,2), PoseRT(i,3), PoseRT(i,4));
+for i = 1:numFiles
+  poseTQFileName = sprintf('/Users/jimmy/Desktop/images/TUM_SLAM/freiburg3_long_office_household/seq-01/frame-%06d.pose.quaternion.txt', i-1);
+  PoseTQ=load(poseTQFileName);
+  PoseRT1=PoseTQ2PoseRT(PoseTQ);
+  PoseRT=inv(PoseRT1);
+  poseRTFileName = sprintf('/Users/jimmy/Desktop/images/TUM_SLAM/freiburg3_long_office_household/seq-01/frame-%06d.pose.txt', i-1);
+  fileID = fopen(poseRTFileName,'w');
+  for j=1:4
+    fprintf(fileID, '%.10f %.10f %.10f %10f \n', PoseRT(j,1), PoseRT(j,2), PoseRT(j,3), PoseRT(j,4));
+  end
+  fclose(fileID);
 end
+
+
+
+
