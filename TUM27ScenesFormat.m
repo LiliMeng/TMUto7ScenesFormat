f152ebe9666279f2c1fb3a89f5a067c1ca1003e8
LiliMeng <lilimeng1103@gmail.com>
2016-07-09 17:52:17 -0700

    Update TUM27ScenesFormat.m

--- TUM27ScenesFormat.m
+++ TUM27ScenesFormat.m
@@ -1,11 +1,11 @@
 close all
 clear all
 
-PoseFileName=dir('/Users/jimmy/Desktop/images/TUM_SLAM/freiburg3_long_office_household/seq-01/*.txt');
+%PoseFileName=dir('/Users/jimmy/Desktop/images/TUM_SLAM/freiburg3_long_office_household/seq-01/*.txt');
 
-numFiles=size(PoseFileName,1);
+%numFiles=size(PoseFileName,1);
 
-for i = 1:numFiles
+for i = 1:2486
   poseTQFileName = sprintf('/Users/jimmy/Desktop/images/TUM_SLAM/freiburg3_long_office_household/seq-01/frame-%06d.pose.quaternion.txt', i-1);
   PoseTQ=load(poseTQFileName);
   PoseRT1=PoseTQ2PoseRT(PoseTQ);
@@ -17,7 +17,4 @@ for i = 1:numFiles
   end
   fclose(fileID);
 end
-
-
-
 
