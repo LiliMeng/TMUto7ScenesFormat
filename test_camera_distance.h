945e1f1351b3ea54ba0eb984a021b6265fcee8a8
LiliMeng <lilimeng1103@gmail.com>
2016-07-10 16:29:21 -0700

    Create test_camera_distance.h

---
+++ test_camera_distance.h
@@ -0,0 +1,50 @@
+//
+//  test_camera_distance.h
+//  RGBRegressionForest
+//
+//  Created by Lili on 7/3/16.
+//  Copyright (c) 2016 Nowhere Planet. All rights reserved.
+//
+
+#ifndef __RGBRegressionForest__test_camera_distance__
+#define __RGBRegressionForest__test_camera_distance__
+
+
+#include "UT_SC_regression.hpp"
+#include "SCRF_tree.hpp"
+#include <iostream>
+#include <fstream>
+#include "SCRF_regressor_builder.hpp"
+#include "SCRF_regressor.hpp"
+#include "RGBG_tree.hpp"
+#include "RGBGUtil.hpp"
+#include "RGBGRegressor.hpp"
+#include "RGBGRegressorBuilder.hpp"
+#include "cvxPoseEstimation.hpp"
+#include "ms7ScenesUtil.hpp"
+#include "cvxCalib3d.hpp"
+#include "cvxIO.hpp"
+
+using namespace cv;
+using namespace std;
+
+
+void camera_rot_distance(const char* train_file_name,
+                         const char* test_file_name,
+                         const double trans_threshold,
+                         vector<double> &test_min_rot_vec_within_trans_threshold);
+
+void camera_trans_distance(const char* train_file_name,
+                     const char* test_file_name,
+                     const double rot_threshold,
+                     vector<double> &test_min_trans_vec_within_rot_threshold);
+
+void test_camera_distance();
+
+void test_minCameraDistanceUnderAngularThreshold();
+
+void test_min_camera_rotDistanceUnderTransThreshold();
+
+void test_cameraDepthToWorldCoordinate();
+
+#endif /* defined(__RGBRegressionForest__test_camera_distance__) */
